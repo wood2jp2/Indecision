d50c3206eab6292af5f71b890ea7a7d1c0c0085b
Josh Wood <wood2jp2@gmail.com>
2018-03-28 23:34:29 -0400

    created simple webpack config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+// entry point? app.js
+
+// output for final JS file?
+
+const path = require('path')
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    }
+}
