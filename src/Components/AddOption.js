1a14cd25e74a9a26078647f49bfd334ca4235bb8
Josh Wood <wood2jp2@gmail.com>
2018-04-04 22:59:50 -0400

    webpack config stuff

--- src/Components/AddOption.js
+++ src/Components/AddOption.js
@@ -16,7 +16,7 @@ export default class AddOption extends React.Component {
         const error = this.props.handleAddOption(option)
 
         this.setState(() => ({ error }))
-
+        
         if (!error) {
             e.target.elements.option.value = ''
         }
