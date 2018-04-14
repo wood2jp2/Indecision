32fd460884e0eb11fb2aad89d456f5721c0e90ea
Josh Wood <wood2jp2@gmail.com>
2018-04-14 11:21:48 -0400

    styling done except for modal

--- src/Components/IndecisionApp.js
+++ src/Components/IndecisionApp.js
@@ -79,6 +79,7 @@ export default class IndecisionApp extends React.Component {
                 hasOptions={this.state.options.length > 0}
                 handlePick={this.handlePick}
                 />
+                <div className='widget'>
                 <Options 
                 options={this.state.options}
                 handleDeleteOption={this.handleDeleteOption}
@@ -86,6 +87,7 @@ export default class IndecisionApp extends React.Component {
                 <AddOption 
                 handleAddOption={this.handleAddOption}
                 />
+                </div>
                 </div>
                 <OptionModal
                 selectedOption={this.state.selectedOption}
--- src/Components/Options.js
+++ src/Components/Options.js
@@ -2,17 +2,22 @@ import React from 'react'
 import Option from './Option'
 
 const Options = props => (
+
         <div>
-            {props.options.length === 0 && <p>There are no options to display!</p>}
+            {props.options.length === 0 && <p className='widget__message'>There are no options to display!</p>}
+            <div className='widget-header'>
+            <h3 className='widget-header__title'>Your Options</h3>
             <button 
                onClick={props.handleDeleteOptions}
                className='button button--link'
             >Remove All</button>
+            </div>
             {props.options && props.options.map((opt, i) => (
                 <Option 
                 key={i} 
                 optionText={opt} 
                 handleDeleteOption={props.handleDeleteOption}
+                number={i+1}
                 />
                 ))}
         </div>
