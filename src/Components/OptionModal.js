4fcfa36e121687566cbc481b5ef19b2b4675b789
Josh Wood <wood2jp2@gmail.com>
2018-04-16 22:33:50 -0400

    favicon, styling, mobile-responsive

--- src/Components/OptionModal.js
+++ src/Components/OptionModal.js
@@ -6,10 +6,13 @@ const OptionModal = props => (
         isOpen={!!props.selectedOption}
         onRequestClose={props.handleClearModal}
         contentLabel='Seleceted Option'
+        closeTimeoutMS={4000}
+        className='modal'
         >
-            <h3>Selected Option</h3>
-            {props.selectedOption && <p>{props.selectedOption}</p>}
+            <h3 className='modal__title'>Selected Option</h3>
+            {props.selectedOption && <p className='modal__body'>{props.selectedOption}</p>}
             <button
+            className='button'
             onClick={props.handleClearModal}>Okay</button>
         </Modal>
     )
